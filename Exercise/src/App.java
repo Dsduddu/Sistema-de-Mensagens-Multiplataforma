7f43353a02a7bd96260f76c5c0c21ed3529d4ccc
Eduardo <eduardobrb1@gmail.com>
2026-02-23 11:05:22 -0300

    Versão 1.1.0
    Versão finalizada do projeto.

--- Exercise/src/App.java
+++ Exercise/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MensagemEmail minhaMsg = new MensagemEmail("eduardobrb1@gmail.com");
+
+        minhaMsg.tipoComunicacao();
+        minhaMsg.enviar();
+        minhaMsg.enviar("Pedido entregue do Mercado Livre");
     }
 }
--- Exercise/src/MensagemEmail.java
+++ Exercise/src/MensagemEmail.java
@@ -3,7 +3,7 @@ public class MensagemEmail extends Mensagem{
     public MensagemEmail(String destinatario) {
         super(destinatario);
     }
-
+    
     @Override
     public void enviar() {
         System.out.println("Enviando E-mail para: " + this.destinatario);
