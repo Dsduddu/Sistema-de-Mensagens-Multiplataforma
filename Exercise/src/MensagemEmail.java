f0253da7884aaa37863ab134f374e5ce7b775617
Eduardo <eduardobrb1@gmail.com>
2026-02-23 10:53:04 -0300

    Versão 1.0.2
    Criação da classe MensagemEmail.

---
+++ Exercise/src/MensagemEmail.java
@@ -0,0 +1,15 @@
+public class MensagemEmail extends Mensagem{
+
+    public MensagemEmail(String destinatario) {
+        super(destinatario);
+    }
+
+    @Override
+    public void enviar() {
+        System.out.println("Enviando E-mail para: " + this.destinatario);
+    }
+
+    public void enviar(String assunto) {
+        System.out.println(assunto);
+    }
+}
